fedb641593928b329801c3904e2d5edcba9ae6f0
Daniel Peretz <124188198+danielperetz770@users.noreply.github.com>
2025-03-13 11:04:25 +0200

    Render mine on board

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 const MINE = '💣'
 const EMPTY = ' '
+const FLAG = '🚩'
 //The model board
 var gBoard = []
 // This is an object by which the board size
@@ -20,80 +21,60 @@ var gLevel = {
 // secsPassed: How many seconds passed
 var gGame = {
     isOn: false,
+    isVictory: false,
     revealedCount: 0,
     markedCount: 0,
     secsPassed: 0
 }
 
-
 // Called when page loads
-
 function onInit() {
+    gGame.isOn = true
     gBoard = buildBoard()
     // setMinesNegsCount(gBoard)
     renderBoard(gBoard)
+    closeModal()
 }
-
 // Builds the board
 // Set some mines
 // Call setMinesNegsCount()
 // Return the created board
 function buildBoard() {
     const size = gLevel.SIZE
-    const numMines = gLevel.MINES
     const board = []
 
     for (var i = 0; i < size; i++) {
         board[i] = []
         for (var j = 0; j < size; j++) {
-            board[i][j] = {
+            const cell = {
                 minesAroundCount: 0,
                 isCovered: true,
                 isMine: false,
                 isMarked: false
             }
+            if (i == 0 && j == 3 || i == 3 && j == 1) cell.isMine = true
+            board[i][j] = cell
         }
     }
-    var minesPlaced = 0
-    while (minesPlaced < numMines) {
-        var randRow = getRandomInt(0, size)
-        var randCol = getRandomInt(0, size)
-        if (!board[randRow][randCol].isMine) {
-            board[randRow][randCol].isMine = true
-            minesPlaced++
-        }
-    }
-
     console.table(board)
     return board
 }
-
-
 
 // Count mines around each cell
 // and set the cell's
 // minesAroundCount.
 function setMinesNegsCount(board) {
-    const size = board.length
-    var mineCount = 0
-
-    for (var rowIdx = 0; rowIdx < size; rowIdx++) {
-        for (var colIdx = 0; colIdx < size; colIdx++) {
-            if (board[rowIdx][colIdx].isMine) continue
-
-            for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
-                for (var j = colIdx - 1; j <= colIdx + 1; j++) {
-
-                    if (i < 0 || i >= size || j < 0 || j >= size) continue
-                    if (i === rowIdx && j === colIdx) continue
-                    if (board[i][j].isMine) mineCount++
-                }
-            }
-            board[rowIdx][colIdx].minesAroundCount = mineCount
+    const rows = board.length
+    const cols = board[0].length
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            if (board[i][j].isMine) continue
+            var currCell = board[i][j]
+            currCell.i = i
+            currCell.j = j
         }
     }
 }
-
 
 // Render the board as a <table>
 // to the page
@@ -102,28 +83,24 @@ function renderBoard(board) {
     for (var i = 0; i < board.length; i++) {
         strHTML += '<tr>'
         for (var j = 0; j < board[0].length; j++) {
-
             const cell = board[i][j]
             const className = `cell cell-${i}-${j}`
-
             strHTML += `<td class="${className}" onclick="onCellClicked(this, ${i}, ${j})">
+            
                             ${cell.isMine ? MINE : cell.isCovered ? '' : cell.minesAroundCount}
                         </td>`
         }
         strHTML += '</tr>'
     }
+    onCellMarked()
     const elContainer = document.querySelector('.board')
     elContainer.innerHTML = strHTML // update the DOM
     console.log('board:', board)
 }
 
-
-
 // Called when a cell is clicked
-
 function onCellClicked(elCell, i, j) {
     const cell = gBoard[i][j]
-
     // Uncover the cell
     cell.isCovered = false
     elCell.classList.add('uncovered')
@@ -133,28 +110,49 @@ function onCellClicked(elCell, i, j) {
         elCell.innerHTML = cell.minesAroundCount > 0 ? cell.minesAroundCount : ''
     }
     gGame.revealedCount++
+
 }
 
-
-
+function getEmptyLocation(board) {
+    var emptyLocations = []
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[0].length; j++) {
+            if (board[i][j] === EMPTY) {
+                emptyLocations.push({ i, j })
+            }
+        }
+    }
+    if (!emptyLocations.length) return null
+    var randIdx = getRandomInt(0, emptyLocations.length)
+    return emptyLocations[randIdx]
+}
 // Called when a cell is rightclicked
 // See how you can hide the context
 // menu on right click
 function onCellMarked(elCell) {
+    oncontextmenu = "javascript:alert('success!');return false;"
+}
+
+function expandReveal(board, elCell, i, j) {
 
 }
 
-// When the user clicks a cell with
-// no mines around, reveal not
-// only that cell, but also its
-// neighbors.
-// NOTE: start with a basic
-// implementation that only
-// reveals the non-mine 1st degree
-// neighbors
-// BONUS: Do it like the real
-// algorithm (see description at
-// the Bonuses section below)
-function expandReveal(board, elCell, i, j) {
+function checkGameOver() {
+    renderBoard()
+    isOn = false
+    var msg = gGame.isVictory ? 'You Won!!!' : 'Game Over'
+    openModal(msg)
+}
 
+
+function openModal(msg) {
+    const elModal = document.querySelector('.modal')
+    const elMsg = elModal.querySelector('.msg')
+    elMsg.innerText = msg
+    elModal.style.display = 'block'
+}
+
+function closeModal() {
+    const elModal = document.querySelector('.modal')
+    elModal.style.display = 'none'
 }
